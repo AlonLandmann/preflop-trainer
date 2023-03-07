48ae355cd634a28a7e1e995981d7b8bddaed7d08
Alon Landmann <rafael.landmann@gmail.com>
2023-03-07 01:21:58 +0100

    [add] authentication

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,12 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
 import css from '@/scss/Navbar.module.scss'
 
 export default function Navbar() {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
   return (
     <div className={css.container}>
       <div className={css.center}>
@@ -13,7 +19,14 @@ export default function Navbar() {
           <div>Results</div>
         </nav>
         <div className={css.loginButton}>
-          <button>Login</button>
+          {user
+            ? <button onClick={() => { location.replace('api/auth/logout') }}>
+                Log out
+              </button>
+            : <button onClick={() => { location.replace('api/auth/login') }}>
+                Log in
+              </button>
+          }
         </div>
       </div>
     </div>
